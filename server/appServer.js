bd16789f1a5ffe5506323e550b0b6f13b4d77583
IgnacioCruz94 <ignacio.cruz@theksquaregroup.com>
2021-11-26 15:10:35 -0600

    commit - server updated

--- server/appServer.js
+++ server/appServer.js
@@ -1,7 +1,7 @@
 // Modules
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const routes = require('./routes/router');
 
 // Middleware to parse body
